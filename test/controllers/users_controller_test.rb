b8665877592037cd77aead23083ef2f18ca2517a
Nicholas Holley <nick@zcid.co>
2014-12-10 10:07:57 -0700

    Add signup error test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,12 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should display errors" do
+    user_data = {name: 'Some name', email: 'email@examplecom', password:
+                 'foobar', password_confirmation: 'foobar'}
+    post :create, user: user_data
+    assert_select 'div#error_explanation' do
+      assert_select 'li', { count: 1}
+    end
+  end
 end
